e2f7f5b422aed69bfb421731841444c25ac03be0
Martine <marty_notes@outlook.com>
2023-10-19 15:28:42 +0200

    Auto stash before revert of "added 'blog' route and page"

---
+++ components/elements/breadcrumb/Breadcrumb.jsx
@@ -0,0 +1,27 @@
+import React, { Children } from 'react'
+
+const Breadcrumb = ({ children }) => {
+  const childrenArray = Children.toArray(children);
+
+  console.log("childrenArray", childrenArray);
+
+  const childrenWtihSeperator = childrenArray.map((child, index) => {
+    if (index !== childrenArray.length - 1) {
+      return (
+        <React.Fragment key={index}>
+          {child}
+          <span>/</span>
+        </React.Fragment>
+      );
+    }
+    return child
+  });
+
+  return (
+    <div className='breadcrumb'>
+      <ol>{childrenWtihSeperator}</ol>
+    </div>
+  )
+}
+
+export default Breadcrumb
